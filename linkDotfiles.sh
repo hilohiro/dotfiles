d0b3b7c4a2759e503858c5da34e84f99aef26340
HIRONO <hilohiro@users.noreply.github.com>
2015-01-12 16:19:13 +0900

    Modified to link files in the git repository.

--- linkDotfiles.sh
+++ linkDotfiles.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
-for dotfile in `ls -1dB .*|grep -v .*\.swp$`
+for dotfile in `git ls-files .\*`
 do
   if ! [ -e ~/$dotfile ] ; then
+    echo ln -s `pwd`/$dotfile ~/$dotfile
     ln -s `pwd`/$dotfile ~/$dotfile
   fi
 done
